ce145333199d836b3a20be236f75784b94134532
Aaron McKain <maaron41602@gmail.com>
2023-05-28 23:38:05 -0500

    Created a welcome to view

--- RantingApp/Views/HomeScreenView.swift
+++ RantingApp/Views/HomeScreenView.swift
@@ -17,11 +17,11 @@ struct HomeScreenView: View {
     let fetchTimer = Timer.publish(every: 3600, on: .main, in: .common).autoconnect()
 
     //For testing regarding the onboarding
-    /*
+    
     init(){
         shouldShowOnboarding = true
     }
-    */
+    
      
     var body: some View {
         NavigationView{
@@ -82,11 +82,34 @@ struct HomeScreenView: View {
                         .frame(width: UIScreen.main.bounds.width * 0.95, height: UIScreen.main.bounds.height * 0.23)
                         .background(Color.white)
                         .cornerRadius(20)
-                        Divider()
                         
                         //Calendar
-                        Text("Calender")
+                        VStack{
+                            Text("Calender")
+                                .font(.title2)
+                                .padding()
+                            HStack{
+                                Text("Select day")
+                                    .font(.caption)
+                                    .padding(.leading, 20)
+                                Spacer()
+                            }
+                            HStack{
+                                let days = ["S", "M", "T", "W", "T", "F", "S"]
+                                ForEach(0..<7) { index in
+                                    ZStack {
+                                        Circle()
+                                            .stroke(Color.gray, lineWidth: 1)
+                                            .frame(width: 40, height: 40)
+                                        Text(days[index])
+                                    }
+                                }
+                            }
                             .padding()
+                        }
+                        .frame(width: UIScreen.main.bounds.width * 0.95, height: UIScreen.main.bounds.height * 0.23)
+                        .background(Color.white)
+                        .cornerRadius(20)
                         
                         //Recent Rants
                         VStack{
@@ -111,7 +134,7 @@ struct HomeScreenView: View {
                     Spacer()
                 }
             }
-            .onReceive(fetchTimer){ _ in
+            .onAppear{
                 fetchAffirmation()
             }
         }
